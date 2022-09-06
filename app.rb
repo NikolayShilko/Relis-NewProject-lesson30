e6d3aa4a7bb6189c844a201e81aec6831b2a5947
nikos <nikos@localhost.localdomain>
2022-09-06 17:21:44 +0300

    трушный способ записи в базу данных

--- app.rb
+++ app.rb
@@ -23,17 +23,18 @@ get '/visit' do
 	erb :visit
 end
 post '/visit' do
-	@username=params[:username]
-	@phone = params[:phone]
-    @datetime = params[:datetime]
-     @barber = params[:barber]
-    c=Client.new
-    c.name=@username
-    c.phone=@phone
-    c.datestamp=@datetime
-    c.barber=@barber
-    c.save
-
+#	@username=params[:username]
+#	@phone = params[:phone]
+ #   @datetime = params[:datetime]
+  #   @barber = params[:barber]
+  #  c=Client.new
+  #  c.name=@username
+  #  c.phone=@phone
+  #  c.datestamp=@datetime
+  #  c.barber=@barber
+  #  c.save
+  c=Client.new params[:client]
+  c.save 
  
   erb "Уважаемый #{@username},ваш номер телефона #{@phone} мы ждём вас #{@datetime} у выбранного парикмахера #{@barber}."
 	
